577156aa12ac6c49b1f54872ba3997e8c45bccac
Carlos Sandoval <csandovalc@unal.edu.co>
2021-12-17 01:56:37 -0500

    Creacion clase encargada de administrar la pieza en pantalla

---
+++ Clases/Figura.js
@@ -0,0 +1,70 @@
+class Figura extends Tetromino {
+	constructor() {
+		super();
+		this._xPos = COORDENADA.origenX;
+		this._yPos = COORDENADA.origenY;
+		this.__figuraEnJuego = this.__crearCuadricula();
+	}
+
+	__crearNuevo() {
+		this._xPos = COORDENADA.origenX;
+		this._yPos = COORDENADA.origenY;
+		this.__figuraEnJuego = this.__crearCuadricula(super.__nuevoMolde());
+	}
+
+	__crearCuadricula(molde = super.definicionMolde()) {
+		return createQuadrille(super.anchoMatriz, molde);
+	}
+
+	dibujar() {
+		drawQuadrille(this.__figuraEnJuego, {
+			x: this._xPos,
+			y: this._yPos,
+			cellLength: VAR_MATH.tamanoCeldas,
+			outline: VAR_CANVA.borde,
+		});
+	}
+
+	__fueraTablero() {
+		if (
+			this._xPos < 0 ||
+			this._xPos >= VAR_MATH.columnas + 1 - super.anchoMatriz ||
+			this._yPos >= VAR_MATH.filas + 1 - super.altoMatriz
+		) {
+			return true;
+		}
+		return false;
+	}
+	__colision() {
+		return false;
+	}
+	__verificacionPosicionValida() {
+		if (!this.__fueraTablero() && !this.__colision()) {
+			return true;
+		}
+		return false;
+	}
+
+	izquierda() {
+		this._xPos--;
+		if (!this.__verificacionPosicionValida()) {
+			this.derecha();
+		}
+	}
+	derecha() {
+		this._xPos++;
+		if (!this.__verificacionPosicionValida()) {
+			this.izquierda();
+		}
+	}
+	__arriba() {
+		this._yPos--;
+		// this.__crearNuevo();
+	}
+	abajo() {
+		this._yPos++;
+		if (!this.__verificacionPosicionValida()) {
+			this.__arriba();
+		}
+	}
+}
