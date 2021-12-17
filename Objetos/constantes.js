e7af513ca8b3ad0df9967582f7ee52e486c52946
Carlos Sandoval <csandovalc@unal.edu.co>
2021-12-17 01:55:06 -0500

    Creacion de un nuevo objeto que maneja las coordenadas

--- Objetos/constantes.js
+++ Objetos/constantes.js
@@ -8,4 +8,10 @@ const VAR_CANVA = {
 	ancho: VAR_MATH.columnas * VAR_MATH.tamanoCeldas,
 	alto: VAR_MATH.filas * VAR_MATH.tamanoCeldas,
 	color: [25, 255, 180].slice(),
+	borde: "#000",
+};
+
+const COORDENADA = {
+	origenX: 3,
+	origenY: 0,
 };
