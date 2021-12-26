485604191d38364bdf1620b6a0d7530de0f0dd17
Carlos Sandoval <csandovalc@unal.edu.co>
2021-12-26 15:53:09 -0500

    HardDrop, creacion y aplicacion de getters y setters

--- Clases/Figura.js
+++ Clases/Figura.js
@@ -6,10 +6,29 @@ class Figura extends Tetromino {
 		this.__figuraEnJuego = this.__crearCuadricula();
 	}
 
+	get xPos() {
+		return this._xPos;
+	}
+	set xPos(valor = this._xPos) {
+		this._xPos = valor;
+	}
+	get yPos() {
+		return this._yPos;
+	}
+	set yPos(valor = this._yPos) {
+		this._yPos = valor;
+	}
+	get figuraEnJuego() {
+		return this.__figuraEnJuego;
+	}
+	set figuraEnJuego(valor = this.__figuraEnJuego) {
+		this.__figuraEnJuego = valor;
+	}
+
 	__crearNuevo() {
-		this._xPos = COORDENADA.origenX;
-		this._yPos = COORDENADA.origenY;
-		this.__figuraEnJuego = this.__crearCuadricula(super.__nuevoMolde());
+		this.xPos = COORDENADA.origenX;
+		this.yPos = COORDENADA.origenY;
+		this.figuraEnJuego = this.__crearCuadricula(super.__nuevoMolde());
 	}
 
 	__crearCuadricula(molde = super.definicionMolde()) {
@@ -17,9 +36,9 @@ class Figura extends Tetromino {
 	}
 
 	dibujar() {
-		drawQuadrille(this.__figuraEnJuego, {
-			x: this._xPos,
-			y: this._yPos,
+		drawQuadrille(this.figuraEnJuego, {
+			x: this.xPos,
+			y: this.yPos,
 			cellLength: VAR_MATH.tamanoCeldas,
 			outline: VAR_CANVA.borde,
 			board: true,
@@ -29,9 +48,9 @@ class Figura extends Tetromino {
 	__fueraTablero() {
 		const indicesTablero = [VAR_MATH.columnas + 1, VAR_MATH.filas + 1];
 		if (
-			this._xPos < 0 ||
-			this._xPos >= indicesTablero[0] - super.anchoMatriz ||
-			this._yPos >= indicesTablero[1] - super.altoMatriz
+			this.xPos < 0 ||
+			this.xPos >= indicesTablero[0] - super.anchoMatriz ||
+			this.yPos >= indicesTablero[1] - super.altoMatriz
 		) {
 			return true;
 		}
@@ -48,43 +67,50 @@ class Figura extends Tetromino {
 	}
 
 	izquierda() {
-		this._xPos--;
+		this.xPos--;
 		if (!this.__verificacionPosicionValida()) {
 			this.derecha();
 		}
 	}
 	derecha() {
-		this._xPos++;
+		this.xPos++;
 		if (!this.__verificacionPosicionValida()) {
 			this.izquierda();
 		}
 	}
 	__arriba() {
-		this._yPos--;
+		this.yPos--;
 		this.__crearNuevo();
 	}
 	abajo() {
-		this._yPos++;
+		this.yPos++;
 		if (!this.__verificacionPosicionValida()) {
 			this.__arriba();
 		}
 	}
+	hardDrop() {
+		while (this.__verificacionPosicionValida()) {
+			this.yPos++;
+		}
+		this.yPos--;
+	}
 
 	__actualizacionTamanoMatriz() {
-		let temp = this.__figuraEnJuego.toMatrix();
+		let temp = this.figuraEnJuego.toMatrix();
 		super.anchoMatriz = obtenerAnchoMatriz(temp);
 		super.altoMatriz = obtenerAltoMatriz(temp);
 	}
 	rotacionIzquierda() {
-		this.__figuraEnJuego.rotate();
+		this.figuraEnJuego.rotate();
 		this.__actualizacionTamanoMatriz();
 		if (!this.__verificacionPosicionValida()) {
 			this.rotacionDerecha();
 		}
 	}
 	rotacionDerecha() {
+		// 3 debido a que simula una rotacion inversa
 		for (let i = 0; i < 3; i++) {
-			this.__figuraEnJuego.rotate();
+			this.figuraEnJuego.rotate();
 		}
 		this.__actualizacionTamanoMatriz();
 		if (!this.__verificacionPosicionValida()) {
--- Clases/Tetromino.js
+++ Clases/Tetromino.js
@@ -11,80 +11,77 @@ class Tetromino {
 	get indice() {
 		return this.__indice;
 	}
-	set indice(nuevoValor) {
+	set indice(nuevoValor = this.__indice) {
 		this.__indice = nuevoValor;
 	}
-
 	get color() {
 		return this.__color;
 	}
-	set color(nuevoValor) {
+	set color(nuevoValor = this.__color) {
 		this.__color = nuevoValor;
 	}
-
 	get anchoMatriz() {
 		return this.__anchoMatriz;
 	}
-	set anchoMatriz(nuevoValor) {
+	set anchoMatriz(nuevoValor = this.__anchoMatriz) {
 		this.__anchoMatriz = nuevoValor;
 	}
-
 	get altoMatriz() {
 		return this.__altoMatriz;
 	}
-	set altoMatriz(nuevoValor) {
+	set altoMatriz(nuevoValor = this.__altoMatriz) {
 		this.__altoMatriz = nuevoValor;
 	}
 
 	__nuevoMolde() {
-		this.__indice = numeroAleatorioEntero(0, 7);
+		this.indice = numeroAleatorioEntero(0, 7);
 		return this.definicionMolde();
 	}
 
 	definicionMolde() {
-		switch (this.__indice) {
+		switch (this.indice) {
 			// Figura I
 			case 0:
-				this.__anchoMatriz = 1;
-				this.__altoMatriz = 4;
-				this.__color = color(0, 255, 255, 255);
-				return [this.__color, this.__color, this.__color, this.__color];
+				this.anchoMatriz = 1;
+				this.altoMatriz = 4;
+				this.color = color(0, 255, 255, 255);
+				return [this.color, this.color, this.color, this.color];
 			// Figura J
 			case 1:
-				this.__anchoMatriz = 2;
-				this.__altoMatriz = 3;
-				this.__color = color(0, 0, 255, 255);
-				return [0, this.__color, 0, this.__color, this.__color, this.__color];
+				this.anchoMatriz = 2;
+				this.altoMatriz = 3;
+				this.color = color(0, 0, 255, 255);
+				return [0, this.color, 0, this.color, this.color, this.color];
 			// Figura L
 			case 2:
-				this.__anchoMatriz = 2;
-				this.__altoMatriz = 3;
-				this.__color = color(255, 165, 0, 255);
-				return [this.__color, 0, this.__color, 0, this.__color, this.__color];
+				this.anchoMatriz = 2;
+				this.altoMatriz = 3;
+				this.color = color(255, 165, 0, 255);
+				return [this.color, 0, this.color, 0, this.color, this.color];
 			// Figura O
 			case 3:
-				this.__anchoMatriz = 2;
-				this.__altoMatriz = 2;
-				this.__color = color(255, 255, 0, 255);
-				return [this.__color, this.__color, this.__color, this.__color];
+				this.anchoMatriz = 2;
+				this.altoMatriz = 2;
+				this.color = color(255, 255, 0, 255);
+				return [this.color, this.color, this.color, this.color];
 			// Figura S
 			case 4:
-				this.__anchoMatriz = 3;
-				this.__altoMatriz = 2;
-				this.__color = color(255, 0, 0, 255);
-				return [0, this.__color, this.__color, this.__color, this.__color, 0];
+				this.anchoMatriz = 3;
+				this.altoMatriz = 2;
+				this.color = color(255, 0, 0, 255);
+				return [0, this.color, this.color, this.color, this.color, 0];
 			// Figura T
 			case 5:
-				this.__anchoMatriz = 3;
-				this.__altoMatriz = 2;
-				this.__color = color(255, 0, 255, 255);
-				return [0, this.__color, 0, this.__color, this.__color, this.__color];
+				this.anchoMatriz = 3;
+				this.altoMatriz = 2;
+				this.color = color(255, 0, 255, 255);
+				return [0, this.color, 0, this.color, this.color, this.color];
 			// Figura Z
 			case 6:
-				this.__anchoMatriz = 3;
-				this.__altoMatriz = 2;
-				this.__color = color(0, 128, 0, 255);
-				return [this.__color, this.__color, 0, 0, this.__color, this.__color];
+				this.anchoMatriz = 3;
+				this.altoMatriz = 2;
+				this.color = color(0, 128, 0, 255);
+				return [this.color, this.color, 0, 0, this.color, this.color];
 		}
 	}
 }
