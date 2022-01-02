ddf04225a8a118b3b9e756e7366fe111575e0db0
Carlos Sandoval <csandovalc@unal.edu.co>
2022-01-02 17:36:46 -0500

    Organizacion codigo

--- Clases/Figura.js
+++ Clases/Figura.js
@@ -4,8 +4,8 @@ class Figura extends Tetromino {
 		this._xPos = COORDENADA.origenX;
 		this._yPos = COORDENADA.origenY;
 		this._yFantasmaPos = COORDENADA.origenY;
-		this.__figuraEnJuego = this.__crearCuadricula()[0];
-		this.__fantasma = this.__crearCuadricula()[1];
+		this.__figuraEnJuego = this.__crearFiguras()[0];
+		this.__fantasma = this.__crearFiguras()[1];
 		this.__puntaje = 0;
 		this.__nivel = 1;
 	}
@@ -22,12 +22,26 @@ class Figura extends Tetromino {
 	set yPos(valor = this._yPos) {
 		this._yPos = valor;
 	}
+	get yFantasmaPos() {
+		return this._yFantasmaPos;
+	}
+	set yFantasmaPos(valor = this._yFantasmaPos) {
+		this._yFantasmaPos = valor;
+	}
+
 	get figuraEnJuego() {
 		return this.__figuraEnJuego;
 	}
 	set figuraEnJuego(valor = this.__figuraEnJuego) {
 		this.__figuraEnJuego = valor;
 	}
+	get fantasma() {
+		return this.__fantasma;
+	}
+	set fantasma(valor = this.__fantasma) {
+		this.__fantasma = valor;
+	}
+
 	get puntaje() {
 		return this.__puntaje;
 	}
@@ -40,66 +54,33 @@ class Figura extends Tetromino {
 	set nivel(valor = this.__nivel) {
 		this.__nivel = valor;
 	}
-	get fantasma() {
-		return this.__fantasma;
-	}
-	set fantasma(valor = this.__fantasma) {
-		this.__fantasma = valor;
-	}
-	get yFantasmaPos() {
-		return this._yFantasmaPos;
-	}
-	set yFantasmaPos(valor = this._yFantasmaPos) {
-		this._yFantasmaPos = valor;
-	}
 
-	__crearNuevo() {
-		this.xPos = COORDENADA.origenX;
-		this.yPos = COORDENADA.origenY;
-		let moldes = this.__crearCuadricula(super.__nuevoMolde());
-		this.figuraEnJuego = moldes[0];
-		this.fantasma = moldes[1];
-	}
-
-	__crearCuadricula(molde = super.moldeFigura()) {
+	__crearFiguras(molde = super.moldeFiguras()) {
 		return [
 			createQuadrille(super.anchoMatriz, molde[0]),
 			createQuadrille(super.anchoMatriz, molde[1]),
 		];
 	}
-	/* 
-	En versiones anteriores de Quadrille se usaban parametros x - y, desde la version 0.9.0 
-	se emplea el manejo de columnas y filas, debido a esto ocurrian incongruencias con versiones
-	anteriores del codigo, desde ahora x = col, y = row
-	*/
-	__dibujar() {
-		drawQuadrille(this.figuraEnJuego, {
-			col: this.xPos,
-			row: this.yPos,
-			cellLength: VAR_MATH.tamanoCeldas,
-			outline: VAR_CANVA.bordeFigura,
-		});
-		this.__hardDropFantasma();
-		drawQuadrille(this.fantasma, {
-			col: this.xPos,
-			row: this.yFantasmaPos,
-			cellLength: VAR_MATH.tamanoCeldas,
-			outline: VAR_CANVA.bordeFigura,
-		});
+
+	__crearNuevo() {
+		this.xPos = COORDENADA.origenX;
+		this.yPos = COORDENADA.origenY;
+		let moldes = this.__crearFiguras(super.nuevoMolde());
+		this.figuraEnJuego = moldes[0];
+		this.fantasma = moldes[1];
 	}
 
-	iniciar() {
-		this.__dibujar();
-		if (!pausa) {
-			this.__caidaFigura(frameCount);
-		}
-		if (!derrota) {
-			derrota = this.__perder();
-		} else {
-			if (parseInt(Almacenamiento.obtenerTopScore(), 10) < this.puntaje) {
-				Almacenamiento.guardarTopScore(this.puntaje);
-			}
-		}
+	__actualizacionTamanoMatriz() {
+		let temp = this.figuraEnJuego.toMatrix();
+		super.anchoMatriz = Externo.obtenerAnchoMatriz(temp);
+		super.altoMatriz = Externo.obtenerAltoMatriz(temp);
+		let aux = this.fantasma.toMatrix();
+		super.anchoMatriz = Externo.obtenerAnchoMatriz(aux);
+		super.altoMatriz = Externo.obtenerAltoMatriz(aux);
+	}
+
+	__almacenarPieza() {
+		Tablero.guardadoPieza(this.figuraEnJuego, { y: this.yPos, x: this.xPos });
 	}
 
 	__fueraTablero() {
@@ -122,7 +103,6 @@ class Figura extends Tetromino {
 			this.figuraEnJuego,
 			posicion
 		);
-
 		for (let fila of matrizComparativa) {
 			for (let valor of fila) {
 				if (valor != 0) {
@@ -169,17 +149,6 @@ class Figura extends Tetromino {
 		}
 		this.yPos--;
 	}
-
-	__actualizacionTamanoMatriz() {
-		let temp = this.figuraEnJuego.toMatrix();
-		super.anchoMatriz = Externo.obtenerAnchoMatriz(temp);
-		super.altoMatriz = Externo.obtenerAltoMatriz(temp);
-
-		let aux = this.fantasma.toMatrix();
-		super.anchoMatriz = Externo.obtenerAnchoMatriz(aux);
-		super.altoMatriz = Externo.obtenerAltoMatriz(aux);
-	}
-
 	rotacionIzquierda() {
 		this.figuraEnJuego.rotate();
 		this.fantasma.rotate();
@@ -200,32 +169,6 @@ class Figura extends Tetromino {
 		}
 	}
 
-	__almacenarPieza() {
-		Tablero.guardadoPieza(this.figuraEnJuego, { y: this.yPos, x: this.xPos });
-	}
-
-	__aumentarPuntaje(lineasBorradas = 0) {
-		const puntosLinea = 100,
-			denominadorFraccionPorcentaje = 10;
-		let puntajeTemporal = lineasBorradas * puntosLinea;
-		this.puntaje +=
-			puntajeTemporal +
-			puntajeTemporal * (lineasBorradas / denominadorFraccionPorcentaje);
-	}
-
-	__limpiezalinea() {
-		let contador = 0;
-		for (let i = 0; i < VAR_MATH.filas; i++) {
-			let fila = Tablero.busquedaFila();
-			if (fila != undefined) {
-				Tablero.limpiarLinea(fila);
-				contador += 1;
-				this.__caidaTablero(fila);
-			}
-		}
-		this.__aumentarPuntaje(contador);
-	}
-
 	__caidaTablero(fila) {
 		let inicioRecorrido = fila,
 			tablero = Tablero.obtener(),
@@ -244,32 +187,12 @@ class Figura extends Tetromino {
 		this.__cambioNivel();
 	}
 
-	__cambioNivel() {
-		const SIMPLIFICACION_A_DECENAS = 1000,
-			EVITAR_NIVEL_CERO = 1;
-		this.nivel = Math.floor(
-			this.puntaje / SIMPLIFICACION_A_DECENAS + EVITAR_NIVEL_CERO
-		);
-	}
-
-	__perder() {
-		let mapa = Tablero.obtener();
-		mapa = mapa.toMatrix();
-		for (let col of mapa[0]) {
-			if (col != 0) {
-				return true;
-			}
-		}
-		return false;
-	}
-
 	__posicionValidaFantasma() {
 		if (!this.__fueraFantasma() && !this.__colisionFantasma()) {
 			return true;
 		}
 		return false;
 	}
-
 	__fueraFantasma() {
 		const indicesTablero = [VAR_MATH.columnas + 1, VAR_MATH.filas + 1];
 		if (
@@ -281,14 +204,12 @@ class Figura extends Tetromino {
 		}
 		return false;
 	}
-
 	__colisionFantasma() {
 		let posicion = {
 			y: this.yFantasmaPos,
 			x: this.xPos,
 		};
 		let matrizComparativa = Tablero.comparacionPieza(this.fantasma, posicion);
-
 		for (let fila of matrizComparativa) {
 			for (let valor of fila) {
 				if (valor != 0) {
@@ -298,11 +219,84 @@ class Figura extends Tetromino {
 		}
 		return false;
 	}
-
 	__hardDropFantasma() {
 		while (this.__posicionValidaFantasma()) {
 			this.yFantasmaPos++;
 		}
 		this.yFantasmaPos--;
+	}
+
+	__limpiezalinea() {
+		let contador = 0;
+		for (let i = 0; i < VAR_MATH.filas; i++) {
+			let fila = Tablero.busquedaFila();
+			if (fila != undefined) {
+				Tablero.limpiarLinea(fila);
+				contador += 1;
+				this.__caidaTablero(fila);
+			}
+		}
+		this.__aumentarPuntaje(contador);
+	}
+
+	__aumentarPuntaje(lineasBorradas = 0) {
+		const puntosLinea = 100,
+			denominadorFraccionPorcentaje = 10;
+		let puntajeTemporal = lineasBorradas * puntosLinea;
+		this.puntaje +=
+			puntajeTemporal +
+			puntajeTemporal * (lineasBorradas / denominadorFraccionPorcentaje);
+	}
+
+	__cambioNivel() {
+		const SIMPLIFICACION_A_DECENAS = 1000,
+			EVITAR_NIVEL_CERO = 1;
+		this.nivel = Math.floor(
+			this.puntaje / SIMPLIFICACION_A_DECENAS + EVITAR_NIVEL_CERO
+		);
+	}
+
+	/* 
+	En versiones anteriores de Quadrille se usaban parametros x - y, desde la version 0.9.0 
+	se emplea el manejo de columnas y filas, debido a esto ocurrian incongruencias con versiones
+	anteriores del codigo, desde ahora x = col, y = row
+	*/
+	__dibujar() {
+		drawQuadrille(this.figuraEnJuego, {
+			col: this.xPos,
+			row: this.yPos,
+			cellLength: VAR_MATH.tamanoCeldas,
+			outline: VAR_CANVA.bordeFigura,
+		});
+		this.__hardDropFantasma();
+		drawQuadrille(this.fantasma, {
+			col: this.xPos,
+			row: this.yFantasmaPos,
+			cellLength: VAR_MATH.tamanoCeldas,
+			outline: VAR_CANVA.bordeFigura,
+		});
+	}
+	iniciar() {
+		this.__dibujar();
+		if (!pausa) {
+			this.__caidaFigura(frameCount);
+		}
+		if (!derrota) {
+			derrota = this.__perder();
+		} else {
+			if (parseInt(Almacenamiento.obtenerTopScore(), 10) < this.puntaje) {
+				Almacenamiento.guardarTopScore(this.puntaje);
+			}
+		}
+	}
+	__perder() {
+		let mapa = Tablero.obtener();
+		mapa = mapa.toMatrix();
+		for (let col of mapa[0]) {
+			if (col != 0) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- Clases/Tetromino.js
+++ Clases/Tetromino.js
@@ -4,7 +4,7 @@ class Tetromino {
 		// Por esa razon se maneja en n+1 a pesar de iniciar desde 0
 		this.__indice = Externo.numeroAleatorioEntero(0, 7);
 		this.__color = undefined;
-		this.__colFantasma = undefined;
+		this.__colorFantasma = undefined;
 		this.__anchoMatriz = undefined;
 		this.__altoMatriz = undefined;
 	}
@@ -21,11 +21,11 @@ class Tetromino {
 	set color(nuevoValor = this.__color) {
 		this.__color = nuevoValor;
 	}
-	get colFantasma() {
-		return this.__colFantasma;
+	get colorFantasma() {
+		return this.__colorFantasma;
 	}
-	set colFantasma(nuevoValor = this.__colFantasma) {
-		this.__colFantasma = nuevoValor;
+	set colorFantasma(nuevoValor = this.__colorFantasma) {
+		this.__colorFantasma = nuevoValor;
 	}
 	get anchoMatriz() {
 		return this.__anchoMatriz;
@@ -40,12 +40,66 @@ class Tetromino {
 		this.__altoMatriz = nuevoValor;
 	}
 
-	__nuevoMolde() {
-		this.indice = Externo.numeroAleatorioEntero(0, 7);
-		return this.moldeFigura();
+	__definicionMolde() {
+		switch (this.indice) {
+			// Figura I
+			case 0:
+				this.anchoMatriz = 1;
+				this.altoMatriz = 4;
+				this.color = color(0, 255, 255, 255);
+				this.colorFantasma = color(0, 255, 255, 90);
+				return [1, 1, 1, 1];
+			// Figura J
+			case 1:
+				this.anchoMatriz = 2;
+				this.altoMatriz = 3;
+				this.color = color(0, 0, 255, 255);
+				this.colorFantasma = color(0, 0, 255, 90);
+				return [0, 1, 0, 1, 1, 1];
+			// Figura L
+			case 2:
+				this.anchoMatriz = 2;
+				this.altoMatriz = 3;
+				this.color = color(255, 165, 0, 255);
+				this.colorFantasma = color(255, 165, 0, 90);
+				return [1, 0, 1, 0, 1, 1];
+			// Figura O
+			case 3:
+				this.anchoMatriz = 2;
+				this.altoMatriz = 2;
+				this.color = color(255, 255, 0, 255);
+				this.colorFantasma = color(255, 255, 0, 90);
+				return [1, 1, 1, 1];
+			// Figura S
+			case 4:
+				this.anchoMatriz = 3;
+				this.altoMatriz = 2;
+				this.color = color(255, 0, 0, 255);
+				this.colorFantasma = color(255, 0, 0, 90);
+				return [0, 1, 1, 1, 1, 0];
+			// Figura T
+			case 5:
+				this.anchoMatriz = 3;
+				this.altoMatriz = 2;
+				this.color = color(255, 0, 255, 255);
+				this.colorFantasma = color(255, 0, 255, 90);
+				return [0, 1, 0, 1, 1, 1];
+			// Figura Z
+			case 6:
+				this.anchoMatriz = 3;
+				this.altoMatriz = 2;
+				this.color = color(0, 128, 0, 255);
+				this.colorFantasma = color(0, 128, 0, 90);
+				return [1, 1, 0, 0, 1, 1];
+		}
 	}
 
-	moldeFigura() {
+	nuevoMolde() {
+		this.indice = Externo.numeroAleatorioEntero(0, 7);
+		return this.moldeFiguras();
+	}
+
+	moldeFiguras() {
 		let molde = this.__definicionMolde(),
 			fig = [];
 
@@ -60,65 +114,11 @@ class Tetromino {
 
 		for (let elemento of molde) {
 			if (elemento == 1) {
-				fantasma.push(this.colFantasma);
+				fantasma.push(this.colorFantasma);
 			} else {
 				fantasma.push(0);
 			}
 		}
 		return [fig, fantasma];
-	}
-
-	__definicionMolde() {
-		switch (this.indice) {
-			// Figura I
-			case 0:
-				this.anchoMatriz = 1;
-				this.altoMatriz = 4;
-				this.color = color(0, 255, 255, 255);
-				this.colFantasma = color(0, 255, 255, 100);
-				return [1, 1, 1, 1];
-			// Figura J
-			case 1:
-				this.anchoMatriz = 2;
-				this.altoMatriz = 3;
-				this.color = color(0, 0, 255, 255);
-				this.colFantasma = color(0, 0, 255, 100);
-				return [0, 1, 0, 1, 1, 1];
-			// Figura L
-			case 2:
-				this.anchoMatriz = 2;
-				this.altoMatriz = 3;
-				this.color = color(255, 165, 0, 255);
-				this.colFantasma = color(255, 165, 0, 100);
-				return [1, 0, 1, 0, 1, 1];
-			// Figura O
-			case 3:
-				this.anchoMatriz = 2;
-				this.altoMatriz = 2;
-				this.color = color(255, 255, 0, 255);
-				this.colFantasma = color(255, 255, 0, 100);
-				return [1, 1, 1, 1];
-			// Figura S
-			case 4:
-				this.anchoMatriz = 3;
-				this.altoMatriz = 2;
-				this.color = color(255, 0, 0, 255);
-				this.colFantasma = color(255, 0, 0, 100);
-				return [0, 1, 1, 1, 1, 0];
-			// Figura T
-			case 5:
-				this.anchoMatriz = 3;
-				this.altoMatriz = 2;
-				this.color = color(255, 0, 255, 255);
-				this.colFantasma = color(255, 0, 255, 100);
-				return [0, 1, 0, 1, 1, 1];
-			// Figura Z
-			case 6:
-				this.anchoMatriz = 3;
-				this.altoMatriz = 2;
-				this.color = color(0, 128, 0, 255);
-				this.colFantasma = color(0, 128, 0, 100);
-				return [1, 1, 0, 0, 1, 1];
-		}
 	}
 }
